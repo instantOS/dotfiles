8c44e9d1bae4b2666e2ad906e7147097fea9b564
paperbenni <paperbenni@gmail.com>
2019-03-11 15:33:40 +0100

    adds status messages

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 cd $HOME
-
+echo "installing config files"
 mkdir paperbenni
 cd paperbenni
 rm -rf dotfiles
-git clone --depth=1 https://github.com/paperbenni/dotfiles.git
+git clone --depth=1 https://github.com/paperbenni/dotfiles.git &>/dev/null
 cd dotfiles
 mv .bashrc ~/
+echo "done"
