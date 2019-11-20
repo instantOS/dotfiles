8e50a31e4c35d6c5a385fbe251e8d0ba81441d36
paperbenni <paperbenni@gmail.com>
2019-11-20 14:03:23 +0100

    add roboto font

---
+++ fonts/roboto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+convert -list font | grep -iq 'roboto' && exit
+mkdir -p ~/.local/share/fonts &>/dev/null
+
+pushd .
+cd /tmp
+mkdir roboto
+cd roboto
+wget -O roboto.zip "https://fonts.google.com/download?family=Roboto"
+unzip roboto.zip
+rm LICENSE.txt
+roboto.zip
+mv ./* ~/.local/share/fonts
+popd
