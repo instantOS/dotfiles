565e1ec79e8ec711d51d9a1419971a8bc41cf7db
paperbenni <paperbenni@gmail.com>
2019-11-30 13:43:55 +0100

    fix font path

--- fonts/sfpro.sh
+++ fonts/sfpro.sh
@@ -11,14 +11,14 @@ cd .cache/applefonts
 if ! convert -list font | grep -q 'SF-Pro'; then
     wget https://developer.apple.com/design/downloads/SF-Font-Pro.dmg
     appledmg SF-Font-Pro.dmg
-    mv SanFranciscoPro/Library/Fonts/* ~/.fonts
+    mv Library/Fonts/* ~/.fonts
     rm -rf ./*
 fi
 
 if ! convert -list font | grep -q 'SF-Mono'; then
     wget https://developer.apple.com/design/downloads/SF-Mono.dmg
     appledmg SF-Mono.dmg
-    mv SFMonoFonts/Library/Fonts/* ~/.fonts
+    mv Library/Fonts/* ~/.fonts
 fi
 
 cd
