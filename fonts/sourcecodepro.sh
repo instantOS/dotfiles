77867af802483b89fd67d29e7e0add1b79bb01a7
paperbenni <paperbenni@gmail.com>
2020-03-01 14:09:16 +0100

    add patched sourcecode font

--- fonts/sourcecodepro.sh
+++ fonts/sourcecodepro.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 echo "installing source code pro"
 convert -list font | grep -iq 'source.*code' && exit
-mkdir -p ~/.local/share/fonts &>/dev/null
 
+pushd .
+
+mkdir -p ~/.local/share/fonts &>/dev/null
+cd ~/.local/share/fonts
 echo "installing source code pro"
-wget -qO ~/.local/share/fonts/sourcecode.otf \
+wget -qO sourcecode.otf \
     "https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.otf"
+
+# custom font symbols
+wget "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf"
+wget "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf"
+
+popd
